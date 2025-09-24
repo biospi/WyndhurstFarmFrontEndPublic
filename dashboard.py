82f5fbd9bc160595e2100316c1ee43948b74adb0
fo18103 <fo18103@bristol.ac.uk>
2025-09-24 12:13:22 +0100

    fixed gui size.

--- dashboard.py
+++ dashboard.py
@@ -234,8 +234,9 @@ def save_config(cfg):
 
 root = ttk.Window(themename="cosmo")
 root.title("Wyndhurst Dashboard")
-root.geometry("500x300")
-root.resizable(False, False)
+# root.geometry("500x300")
+root.minsize(600, 300)
+root.resizable(True, True)
 
 # Icon
 try:
